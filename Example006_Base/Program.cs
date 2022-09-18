a47d916641c235fb70123f4ea1771f323263ad09
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-18 14:02:31 +0700

    Example 006 Basee

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write(a);
+Console.Write(" ");
+Console.Write(b);
+Console.Write(" ");
+Console.Write(c);
+Console.Write(" ");
+Console.Write(d);
+Console.Write(" ");
+Console.WriteLine(e);
+Console.Write("max = ");
+Console.Write(max);
