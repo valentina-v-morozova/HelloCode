12d62f41a2fe6b9e73e875c91f283c34ead2e5f2
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-24 12:22:22 +0700

    записала 11 модернизированный пример

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -19,6 +19,31 @@ void PrintArray(int[] col) //выводим массив на экран
         position++;
     }
 }
+
+int IndexOf(int[] collection, int find) //функция поиска номера элемента массива по значению
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //значение не найдено
+
+    while (index < count)
+    {
+        if(collection[index] == find) //если на текущей позиции искомое значение
+                {
+            position = index; //запомнить номер позиции
+            break;
+        }
+        index++;
+    }
+    return position;
+}
 int[] array = new int[10]; //устанавливаем длинну массива
+
 FillArray(array);
+//array[6] = 4;
+
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
