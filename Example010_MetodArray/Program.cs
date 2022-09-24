383da0e167358aa87e9e658ed1bf01b5f09a1baf
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-24 11:32:47 +0700

    added examples 8-1, 9, 10, 11e

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Имеется одномерный массив array из n элементов, требуется
+// найти элемент массива, равный find
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+    }
+    //index = index+1;
+    index++;
+}
