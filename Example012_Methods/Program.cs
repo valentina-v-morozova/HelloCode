59ae4b82c1c27ff9ef018ccb2d0b8f17917a11a7
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-25 13:49:59 +0700

    финальный код 3 лекции

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -66,11 +66,89 @@
 //string res = Method41(10, "asdf ");
 //Console.WriteLine(res);
 
-for (int i = 2; i <= 10; i++)
+//for (int i = 2; i <= 10; i++)
+//{
+//for (int j = 2; j <= 10; j++)
+//{
+//    Console.WriteLine($"{i} x {j} = {i * j}");
+//  }
+//Console.WriteLine();
+//}
+
+//Дан текст. В тексте нужно все пробелы заменить черточками,
+//маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+//маленькими “с”.
+
+//string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+//+ "ежели бы вас послали вместо нашего милого Винценгероде,"
+//+ " вы бы взяли приступом согласие прусского короля. "
+//+ "Вы так красноречивы. Вы дадите мне чаю?";
+//Console.WriteLine(text);
+//
+//string Replace(string text, char oldValue, char newValue)
+//{
+//string result = String.Empty;
+//
+//int length = text.Length; //вычисляем длину строки
+//
+//for (int i = 0; i < length; i++)
+//{
+//if (text[i] == oldValue) result = result + $"{newValue}";
+//else result = result + $"{text[i]}";
+//}
+//
+//return result;
+//}
+//
+//Console.WriteLine();
+//string newText = Replace(text, ' ', '|');
+//Console.WriteLine(newText);
+//
+//Console.WriteLine();
+//string newText1 = Replace(newText, 'с', 'С');
+//Console.WriteLine(newText1);
+//
+//Console.WriteLine();
+//string newText2 = Replace(newText1, 'к', 'К');
+//Console.WriteLine(newText2);
+
+internal class Program
 {
-    for (int j = 2; j <= 10; j++)
+    private static void Main(string[] args)
     {
-        Console.WriteLine($"{i} x {j} = {i * j}");
+        int[] arr = { 1, 5, 4, 2, 3, 2, 5, 7, 1, 1, 9 };
+
+        void PrintArray(int[] array) //задаем метод для вывода массива на экран
+        {
+            int count = array.Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minPosition = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minPosition]) minPosition = j;
+                }
+
+                int temporary = array[i];// записываем значение из текущей рабочей позиции
+                array[i] = array[minPosition];
+                array[minPosition] = temporary;
+            }
+        }
+
+        PrintArray(arr);
+        SelectionSort(arr);
+
+        PrintArray(arr);
     }
-Console.WriteLine();
 }
