68cdba43556a83755f6ad22d39f534ce84df5c45
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-10-01 15:12:24 +0700

    пример массивов

---
+++ Example013_Recursia/Program.cs
@@ -0,0 +1,55 @@
+﻿//String.Empty - инициализация массива
+//table[0, 0] table[0, 1] table[0, 2] table[0, 3] table[0, 4] - обращение к элементам 1 строки
+//table[1, 0] table[1, 1] table[1, 2] table[1, 3] table[1, 4] - обращение к элементам 2 строки
+
+// string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++) //выводим массив на печать
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"{table[rows, columns]}");
+//     }
+// }
+
+/* int[,] matrix = new int[3, 4];
+
+for (int i = 0; i < matrix.GetLength(0); i++) //выводим массив на печать
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+} */
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) //выводим массив на печать
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) //выводим массив на печать
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
